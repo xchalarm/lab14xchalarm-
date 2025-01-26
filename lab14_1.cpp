79623a4038ff4c0e582e24ef30cc84a41edea94b
xchalarm <120165210+xchalarm@users.noreply.github.com>
2025-01-27 00:12:40 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	int j;
+	for( int i = 1; i < N; i++){
+		T temp = d[i];
+		j = i - 1;
+		cout << "Pass " << i << ":";
+		while(j >= 0 && d[j] < temp){
+			d[j + 1] = d[j];
+            j--;
+		}
+		d[j + 1] = temp;
+		for (int i = 0; i < N; i++) cout << d[i] << " ";
+		cout << endl;
+	}
 }
 
 int main(){
